45c23040541e482ec114ffa078645950491912db
mawerb <maorbarzilay8@gmail.com>
2026-09-13 17:53:31 -0700

    Moved port parameter to be passable thru cli

--- multKV/main.go
+++ multKV/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"flag"
+	"log"
+)
+
 func main() {
-	startNode(8080)	
+	port := flag.Int("port", 8080, "Type in a port to start up a node (default: 8080)")
+	flag.Parse()
+
+	node := Node{
+		Port:  *port,
+		Peers: []string{"localhost:8002", "localhost:8003"},
+	}
+
+	if err := node.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
--- multKV/node.go
+++ multKV/node.go
@@ -4,15 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
-func startNode(port int) {
-	address := fmt.Sprintf(":%d", port)
+type Node struct {
+	Port  int
+	Peers []string
+}
+
+func (n *Node) Start() error {
+	address := fmt.Sprintf(":%d", n.Port)
 	ln, err := net.Listen("tcp", address)
 
 	
 	if err != nil {
-		log.Fatalf("could not start server: %v", err)
+		return fmt.Errorf("could not start server: %w", err)
 	}
 	
 	defer ln.Close()
@@ -22,17 +28,55 @@ func startNode(port int) {
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Fatalf("could not accept request: %v", err)
-			continue
-		}	
+			return fmt.Errorf("could not accept request: %w", err)
+		}
 
 		log.Printf("Accepted connection from %v", conn.RemoteAddr())
 
-		go func(c net.Conn) {
-			defer c.Close()
-			
+		handleRead(conn)
+	}
+}
 
-			log.Printf("Sending response to %v", c.RemoteAddr())
-		} (conn)
+func (n *Node) Ping(index int) error {
+	if index < 0 || index >= len(n.Peers) {
+		return fmt.Errorf("Index: %d passed into ping is invalid", index)
+	}
+
+	conn, err := net.DialTimeout("tcp", n.Peers[index], 3 * time.Second)
+
+	if err != nil {
+		return fmt.Errorf("Couldn't connect to node: %w", err)
+	}
+	defer conn.Close()
+
+	time.Sleep(5 * time.Second)
+
+	message := fmt.Sprintf("Hello from node: %d\n", n.Port)
+	
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		return fmt.Errorf("Couldn't send message to node: %w", err)
+	}
+
+	return nil
+}
+
+func read(c net.Conn) error {
+	buffer := make([]byte, 1024)
+
+	n, err := c.Read(buffer)
+	if err != nil {
+		return fmt.Errorf("Read error: %v", err)
+	}
+
+	fmt.Printf("Recieved: %s\n", string(buffer[:n]))
+	return nil
+}
+
+func handleRead(c net.Conn) {
+	defer c.Close()
+
+	if err := read(c); err != nil {
+		log.Printf("connection error: %v", err)
 	}
 }
